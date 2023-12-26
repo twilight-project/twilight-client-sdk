680e8a6729ed9d859b7a42725e1ae5c6c6d8fa9b
usmanshahid86 <no-reply@github.com>
2023-12-27 04:44:42 +0800

    removed errors to create the basic structure

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,17 @@
 
 //! ZkOS Client Wallet implementation.
 
-pub extern crate zkos;
+pub extern crate quisquislib;
+pub extern crate transaction;
+pub extern crate transactionapi;
 pub extern crate zkschnorr;
+pub extern crate zkvm;
 
 mod keys_management;
 pub mod relayer;
 pub mod relayer_types;
-pub mod tx;
+pub mod script;
+pub mod transfer;
 pub mod utxo_util;
 
 //use rand::rngs::OsRng;
