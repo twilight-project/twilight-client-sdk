dfb61f5bcecf9e5b46a09432f94a3b6ce3bc7eb4
Usman Shahid <26981965+usmanshahid86@users.noreply.github.com>
2024-03-14 05:33:22 +0500

    updated createorderprogram

--- src/programcontroller.rs
+++ src/programcontroller.rs
@@ -189,7 +189,9 @@ mod tests {
     // Stack -> C(IM) -> PositionSize-> C(Leverage) -> EntryPrice -> OrderSide -> tx_Data(C(PositionValue))
       pub fn get_trader_order_program() -> Program {
         let order_prog = Program::build(|p| {
-            p.roll(3) // Get Leverage to top of stack
+            p.commit()
+                .expr() // convert positionvalue to expression
+                .roll(3) // Get Leverage to top of stack
                 .commit()
                 .expr()
                 .roll(5) // Get IM to top of stack
